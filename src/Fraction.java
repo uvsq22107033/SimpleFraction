882de364cb22eb7b4907f13b080e82fcc7566025
Talel garma <Talelgarma07@gmail.com>
2021-10-01 14:14:58 +0200

    commit2

--- src/Fraction.java
+++ src/Fraction.java
@@ -1,2 +1,31 @@
+package com.company;
+
 public class Fraction {
+    float a;
+    float b;
+    public void Fraction(float a, float b){
+        this.a=a;
+        this.b=b;
+    }
+    public void Fraction(){
+
+    }
+    public void SetFraction(float a, float b){
+        this.a=a;
+        if(b!=0) {
+            System.out.println("You can't set dénominateur on 0 ");
+
+
+            this.b = b;
+        }
+        else{
+            this.b = 1;
+
+        }
+    }
+    public String toString(){
+        System.out.println("Cette fraction a comme numérateur : %f"+ a+" et comme dénominateur : %f "+ b+ "==>"+a+"/"+b);
+        return null;
+    }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,11 @@
+package com.company;
 public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        System.out.println("----GHILES----");
+        Fraction r = new Fraction();
+        r.SetFraction(5,0);
+        r.printFraction();
+    }
 }
